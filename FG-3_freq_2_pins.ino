7d996e9fae9e7a7ac7dc18faa2df063ba2bd3fa6
schober-stl <67021351+schober-stl@users.noreply.github.com>
2024-01-22 17:56:18 -0600

    Update FG-3_freq_2_pins.ino

--- FG-3_freq_2_pins.ino
+++ FG-3_freq_2_pins.ino
@@ -1,9 +1,9 @@
 /*  This project uses a Teeny 4.0 https://www.pjrc.com/ and 2 FG-3+ fluxgate magnetometers from https://www.fgsensors.com/
- *  Must cut trace that connects Vin to USB on Teensy 4.0 so powered by linear regulaor circuit https://www.pjrc.com/store/teensy40.html 
+ *  Must cut the trace that connects Vin to USB on Teensy 4.0 so powered by linear regulator circuit https://www.pjrc.com/store/teensy40.html 
  *  The Teensy 4.0 and Fg-3+ sensors are powered by the linear regulator circuit in https://www.fgsensors.com/_files/ugd/7034a0_c59536c9022b4a5798fcc6045dcf6841.pdf
- *  Must use logic leveler to convert FG3+ signal from 5.0 V to 3.3 V before connecting to pins on Teensy (pins are not 5.0 V tolerant)
+ *  Must use a logic leveler to convert FG3+ signal from 5.0 V to 3.3 V before connecting to pins on Teensy (pins are not 5.0 V tolerant)
  *  Uses Interrupts to get pulse duration for both pins 6 and 9
- *  MainPeriod contols speed versus precision
+ *  MainPeriod controls speed versus precision
 */
 
 #define MainPeriod 2500 // adjust for tradeoff of precision vs speed
